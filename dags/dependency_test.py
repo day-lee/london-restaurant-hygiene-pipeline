1845dbeb01a829aafa8f7fc9e34ba3c773166d20
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-22 22:45:58 +0100

    Add success criteria

--- dags/dependency_test.py
+++ dags/dependency_test.py
@@ -2,6 +2,12 @@ from airflow import DAG
 from airlfow.operators.python import PythonOperator 
 from datetime import datetime
 import time 
+"""
+Airflow UI의 Graph View와 Gantt View
+Graph View: t1이 끝난 직후 t2와 t3에 동시에 불이 들어오며 병렬로 도는 모습
+
+Gantt View: t2와 t3가 실행되는 동안 t4가 어두운 색(Upstream 상태 대기)으로 기다리고 있다가, 둘 중 더 오래 걸리는 작업이 끝나는 순간 t4가 실행되는 타임라인 시각화
+"""
 
 with DAG(
     dag_id='dependency_test',
@@ -17,12 +23,12 @@ with DAG(
 
     t2_load_s3 = PythonOperator(
         task_id="load_s3_task",
-        python_callable=lambda: (print("s3 업로드 시작"), time.sleep(7), print("s3 업로드 완료")),
+        python_callable=lambda: (print("s3 업로드 시작"), time.sleep(4), print("s3 업로드 완료")),
     )
 
     t3_load_db = PythonOperator(
         task_id="load_to_db",
-        python_callable=lambda: (print("DB 업로드 시작"), time.sleep(7), print("DB 업로드 완료")),
+        python_callable=lambda: (print("DB 업로드 시작"), time.sleep(11), print("DB 업로드 완료")),
     )
 
     t4_cleanup_tmp = PythonOperator(
