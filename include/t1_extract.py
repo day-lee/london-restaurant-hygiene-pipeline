56d1bc4d9df221e9b866ce2218c6bd9f389c8986
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-24 14:17:38 +0100

    Add session and for loop to extract 33 borough file

--- include/t1_extract.py
+++ include/t1_extract.py
@@ -1,29 +1,33 @@
 ### Milestone 1: Task 1 (Extract) – API ➔ Local `/tmp`
 import requests
 
-def extract_xml_to_tmp():
-    print("xml 파일 다운로드 시작")
+LOCAL_AUTHORITY_CODE = ['501', '502', '503', '504', '505', '506', '507', '508', '509', '510', '511', '512', '513', '514', '515', '516', '517', '518', '519', '520', '521', '522', '523', '524', '525', '526', '527', '528', '529', '530', '531', '532', '533']
 
-    url = "https://ratings.food.gov.uk/api/open-data-files/FHRS506en-GB.xml" 
+def extract_xml_to_tmp():
+
+    session = requests.Session() 
     user_headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept": "application/xml"
     }
-    r = requests.get(url, headers=user_headers, timeout=30)
-    try: 
-        r.raise_for_status()
-        # production path
-        with open('/opt/airflow/data/fsa.xml', 'wb') as f:
-        # local path
-        # with open('./data/fsa.xml', 'wb') as f:
-            f.write(r.content)
-        print("xml 파일 /data 에 저장 완료")           
-    except requests.exceptions.HTTPError as e:
-        print("HTTP error occurred:", e) 
-        raise 
-    except requests.exceptions.RequestException as e:
-        print('A request error occurred', e)
-        raise 
+    for code in LOCAL_AUTHORITY_CODE:
+        url = f"https://ratings.food.gov.uk/api/open-data-files/FHRS{code}en-GB.xml" 
+        r = session.get(url, headers=user_headers, timeout=30)
+        try: 
+            r.raise_for_status()
+            print(f"{code} - xml 파일 다운로드 시작")
+            # production path
+            with open(f"/opt/airflow/data/fsa-{code}.xml", 'wb') as f:
+            # local path
+            # with open('./data/fsa.xml', 'wb') as f:
+                f.write(r.content)
+            print(f"{code} - xml 파일 /data 에 저장 완료")           
+        except requests.exceptions.HTTPError as e:
+            print("HTTP error occurred:", e) 
+            raise 
+        except requests.exceptions.RequestException as e:
+            print('A request error occurred', e)
+            raise 
 
 # 로컬 테스트. DAG에서 실행되므로 주석처리 
 # extract_xml_to_tmp()
