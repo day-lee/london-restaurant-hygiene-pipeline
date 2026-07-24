08ed542c7c8e8cff5ac8f9e648b44ad1d6e32208
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-24 13:33:41 +0100

    Add http,network error handling

--- include/t1_extract.py
+++ include/t1_extract.py
@@ -6,9 +6,20 @@ def extract_xml_to_tmp():
 
     url = "https://ratings.food.gov.uk/api/open-data-files/FHRS506en-GB.xml" 
     r = requests.get(url)
-    with open('/opt/airflow/data/fsa.xml', 'wb') as f:
-        f.write(r.content)
-    print("xml 파일 /data 에 저장 완료")
+    try: 
+        r.raise_for_status()
+        # production path
+        with open('/opt/airflow/data/fsa.xml', 'wb') as f:
+        # local path
+        # with open('./data/fsa.xml', 'wb') as f:
+            f.write(r.content)
+        print("xml 파일 /data 에 저장 완료")           
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error occurred:", e) 
+        raise 
+    except requests.exceptions.RequestException as e:
+        print('A request error occurred', e)
+        raise 
 
 # 로컬 테스트. DAG에서 실행되므로 주석처리 
 # extract_xml_to_tmp()
