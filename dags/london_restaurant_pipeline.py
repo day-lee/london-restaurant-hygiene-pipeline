f01a2512648ff505c5e278383ac3196ec110c960
Dayeon Lee <iamdayeonlee@gmail.com>
2026-07-28 23:20:22 +0100

    Add task2 and dependency

--- dags/london_restaurant_pipeline.py
+++ dags/london_restaurant_pipeline.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 from include.t1_extract import extract_xml_to_tmp
+from include.t2_load_to_s3 import load_to_s3
 
 BUCKET_NAME = "london-restaurant-hygiene-data-dy"
 AWS_CONN_ID = "aws_s3_conn" 
@@ -19,7 +20,7 @@ default_args = {
 with DAG(
     'london_restaurant_hygiene_pipeline',
     default_args=default_args,
-    description='step1: XML to S3',
+    description='XML to S3, PostgreSQL',
     schedule_interval='@weekly', # 테스트 목적으로 데일리, 매일 밤 00:00(UTC)에 실행
     catchup=False,
 ) as dag: 
@@ -28,6 +29,14 @@ with DAG(
             task_id="extract_task",
             python_callable=extract_xml_to_tmp,
     )
+    task_2 = PythonOperator (
+            task_id="load_s3_task", 
+            python_callable=load_to_s3,
+    )
+
+    # dependency 설정 
+    task_1 >> task_2
+    
 # # TODO: task_2, task_3, task_4, task_5 정의하고 의존성 연결하기
 #     # task1 끝나야 2,3 가능 
 #     task_1 >> [task_2, task_3]
