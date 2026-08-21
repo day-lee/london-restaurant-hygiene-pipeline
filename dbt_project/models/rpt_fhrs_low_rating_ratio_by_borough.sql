62b4137e24927c1fca07874fd599e517d769f455
Dayeon Lee <iamdayeonlee@gmail.com>
2026-08-21 16:38:00 +0100

    Add updated_at column for rpt

--- dbt_project/models/rpt_fhrs_low_rating_ratio_by_borough.sql
+++ dbt_project/models/rpt_fhrs_low_rating_ratio_by_borough.sql
@@ -1,9 +1,11 @@
-{{ config(materialized='view') 
-}}
+{{ config(
+    materialized='view' 
+) }}
 
 WITH fhrs_summary AS (
     SELECT 
         local_authority_code,
+        MAX(updated_at) AS updated_at,
         COUNT(CASE WHEN rating_score <= 2 THEN 1 END) AS below_2_cnt,
         COUNT(*) AS total_cnt
     FROM {{ ref('fact_hygiene_ratings') }} 
@@ -14,7 +16,8 @@ SELECT
     dim.local_authority_name, 
     f.below_2_cnt, 
     f.total_cnt,
-    ROUND(f.below_2_cnt * 100.0 / f.total_cnt, 2) AS ratio
+    ROUND(f.below_2_cnt * 100.0 / f.total_cnt, 2) AS ratio,
+    f.updated_at
 FROM fhrs_summary f
 JOIN {{ ref('dim_local_authorities') }} dim USING (local_authority_code)
 ORDER BY ratio DESC
